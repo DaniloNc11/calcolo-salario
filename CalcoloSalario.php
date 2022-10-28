6ecde959316a8876189bd25442c890ab2281b89f
DaniloNc11 <finocchio.nicolo@panettipitagora.edu.it>
2022-10-28 16:35:27 +0200

    Form

--- CalcoloSalario.php
+++ CalcoloSalario.php
@@ -1,7 +1,15 @@
 <html>
 <body>
 <form action='<?php echo $_SERVER["PHP_SELF"];?>' method='post'>
-<input type="text" name="Giorni">
-<input type="text" name="OreGiornaliere">
-<input type="text" name="Compenso">
+<b>Giorni</b><br>
+<input type="text" name="Giorni" placeholder="Quanti giorni durera' il lavoro?"><br>
+<b>Ore giornaliere di lavoro</b><br>
+<input type="text" name="OreGiornaliere" placeholder="Quanto lavori al giorno?"><br>
+<b>Compenso orario</b><br>
+<input type="text" name="Compenso" placeholder="Quanto verrai pagato all'ora?"><br>
+<input type="Button" name="CALCOLA">
+</form>
+<?php
+
+?>
 </html>
