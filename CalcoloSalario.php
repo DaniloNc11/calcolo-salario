4ac2d909c4cce1c4ec4f7a26487b8fa3b9824754
DaniloNc11 <finocchio.nicolo@panettipitagora.edu.it>
2022-10-28 16:20:59 +0200

    caselle di testo

---
+++ CalcoloSalario.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<form action='<?php echo $_SERVER["PHP_SELF"];?>' method='post'>
+<input type="text" name="Giorni">
+<input type="text" name="OreGiornaliere">
+<input type="text" name="Compenso">
+</html>
